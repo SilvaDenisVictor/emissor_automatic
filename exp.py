832872d9273a90987daec7ec691de80564d7c754
SilvaDenisVictor <108144928+SilvaDenisVictor@users.noreply.github.com>
2024-11-07 08:50:16 -0300

    Update exp.py

--- exp.py
+++ exp.py
@@ -1,5 +1,4 @@
 class ValorDiferente(Exception):
-    """Exceção personalizada para uma situação específica."""
     def __init__(self, mensagem):
         super().__init__(mensagem)
         self.mensagem = mensagem
